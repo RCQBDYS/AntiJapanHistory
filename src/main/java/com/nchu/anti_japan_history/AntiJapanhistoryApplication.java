dcaede56a824a0b8b5802d87b8f704f587649956
RCQBDYSschool <45528344+RCQBDYSschool@users.noreply.github.com>
2020-03-31 19:07:52 +0800

    message

---
+++ src/main/java/com/nchu/anti_japan_history/AntiJapanhistoryApplication.java
@@ -0,0 +1,13 @@
+package com.nchu.anti_japan_history;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntiJapanhistoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AntiJapanhistoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nchu/anti_japan_history/AntiJapanhistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nchu.anti_japan_history;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntiJapanhistoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
